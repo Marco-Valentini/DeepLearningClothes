dbd2eb45c64e1a4a3ced8e58e3b5e53988dbdf86
Marco Valentini <m.valentini7@studenti.poliba.it>
2023-07-01 02:07:22 +0200

    data preparation to generate catalogue of items and train val and test dataset composed of outfits

---
+++ utility/display.py
@@ -0,0 +1,48 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def display_items(df_reduced):
+    """
+    Given the reduced version of the catalogue, display the first 10 items
+    :param df_reduced:
+    :return:
+    """
+    indeces = list(df_reduced['ID'])[0:10]
+    labels = list(df_reduced['Semantic_category'])[0:10]
+    fig = plt.figure(figsize=(25,20))
+    images=[]
+    for idx in range(10):
+        image = cv2.imread('../dataset/polyvore_outfits/images/' + indeces[idx] + '.jpg')
+        rgbImg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        images.append(rgbImg)
+
+    for idx in range(10):
+        ax = fig.add_subplot(2,int(10/2),idx+1,xticks=[],yticks=[])
+        ax.set_title(labels[idx])
+        plt.imshow(images[idx])
+    plt.show()
+
+
+
+def display_outfits(outfits):
+    for i in range(5):
+        images = []
+        clothes_list = outfits[i]['item_id']
+        img1 = cv2.imread('../dataset/polyvore_outfits/images/' + clothes_list[0] + '.jpg')
+        rgbImg1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+        images.append(rgbImg1)
+        img2 = cv2.imread('../dataset/polyvore_outfits/images/' + clothes_list[1] + '.jpg')
+        rgbImg2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+        images.append(rgbImg2)
+        img3 = cv2.imread('../dataset/polyvore_outfits/images/' + clothes_list[2] + '.jpg')
+        rgbImg3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+        images.append(rgbImg3)
+        img4 = cv2.imread('../dataset/polyvore_outfits/images/' + clothes_list[3] + '.jpg')
+        rgbImg4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
+        images.append(rgbImg4)
+        fig = plt.figure(figsize=(25, 20))
+        for idx in range(4):
+            ax = fig.add_subplot(1, int(4), idx+1, xticks=[], yticks=[])
+            ax.set_title(clothes_list[idx])
+            plt.imshow(images[idx])
+        plt.show()
