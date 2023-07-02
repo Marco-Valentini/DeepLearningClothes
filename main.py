9d63d305bf99bcb548b68cfca404c3340ce693ef
PaoloMasciullo <paolomasciullojz4@gmail.com>
2023-07-02 12:36:45 +0200

    keep track of the image paths of the corresponding embeddings

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-from torchvision.models import resnet18
+from torchvision.models import resnet18, ResNet18_Weights
 import torch.nn as nn
 from torchvision.transforms import transforms
 
@@ -14,7 +14,7 @@ device = torch.device("mps" if torch.has_mps else "cpu")
 # first step: obtain the embeddings of the dataset using the fine-tuned model finetuned_fashion_resnet18.pth
 
 # load the model finetuned_fashion_resnet18.pth
-model = resnet18(pretrained=True)
+model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 num_ftrs = model.fc.in_features  # get the number of input features for the last fully connected layer
 model.fc = nn.Linear(num_ftrs, 4)  # modify the last fully connected layer to have the desired number of classes
 model.load_state_dict(torch.load('./models/finetuned_fashion_resnet18.pth'))  # load the weights of the model finetuned_fashion_resnet18.pth
@@ -32,7 +32,11 @@ image_dataset = ImageFolder(test_dir, transform=data_transform)  # create the da
 
 dataloaders = DataLoader(image_dataset, batch_size=32, shuffle=False, num_workers=0)  # create the dataloader
 
-embeddings, labels = image_to_embedding(dataloaders, model, device)  # get the embeddings of the dataset
+# get the embeddings of the dataset, the labels and the ids
+embeddings, labels, paths = image_to_embedding(dataloaders, model, device)
 # show the embeddings
 print(embeddings)
 print(embeddings.shape)
+print(paths)
+
+# second step: define the bert-like model and train it on the embeddings
