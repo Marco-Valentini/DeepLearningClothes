b6df44cfcd9073788ca447234f57e51dfccfb5b2
PaoloMasciullo <paolomasciullojz4@gmail.com>
2023-07-02 12:36:45 +0200

    keep track of the image paths of the corresponding embeddings

--- image_to_embedding.py
+++ image_to_embedding.py
@@ -1,5 +1,5 @@
 # Description: this file contains the function to convert the images to embeddings using the pretrained model and the
-# dataloader
+# dataloader of the dataset and the device to run the model on.
 import numpy as np
 import torch
 
@@ -10,16 +10,21 @@ def image_to_embedding(dataloader, model, device):
     :param dataloader: the dataloader of the dataset
     :param model: cnn resnet18 model pretrained and finetuned with fashion dataset
     :param device: the device to run the model on
-    :return:
+    :return: the embeddings of the dataset the labels of the dataset and the image paths of the dataset
     """
     # get the embeddings
     embeddings = []
+    # keep track of the images paths
+    paths = []
     with torch.no_grad():  # do not calculate the gradients
         for inputs, labels in dataloader:  # iterate over the data
             inputs = inputs.to(device)  # move the input images to the device
             labels = labels.to(device)  # move the labels to the device
             outputs = model(inputs)  # forward pass
             embeddings.append(outputs.cpu().numpy())  # append the embeddings to the list
+            # Retrieve the image paths from the dataloader dataset
+            batch_ids = dataloader.dataset.samples  # get the image paths
+            paths.extend([path for path, _ in batch_ids])  # append the image paths to the list
     embeddings = np.concatenate(embeddings)  # concatenate the embeddings
 
-    return embeddings, labels
+    return embeddings, labels, paths
