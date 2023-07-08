[WIP] saved embeddings and ID list into files in reduced data folder, changed training code to load pre-computed embeddings and IDs updated umBERT trainer with fix on the computation of the accuracy and of the loss, changed the criterion in training umBERT to work with the logits

BERT_architecture/umBERT.py
@@ -16,12 +16,13 @@ class umBERT(nn.Module):
             nhead=num_heads,
             dim_feedforward=self.dim_feedforward,
             dropout=self.dropout,
-            activation="relu",
+            activation="gelu",
             batch_first=True  # Set batch_first to True
         ), num_layers=num_encoders)  # the encoder stack is a transformer encoder stack
         self.ffnn = nn.Linear(d_model, catalogue_size)  # the output of the transformer is fed to a linear layer
         self.softmax = F.softmax  # the output of the linear layer is fed to a softmax layer
         self.Binary_Classifier = nn.Linear(d_model, 2)  # second task: binary classification
+        self.sigmoid = F.sigmoid  # the output of the binary classifier layer is fed to a sigmoid layer
 
     def forward(self, outfit):
         """

trainings/training_umBERT.py
@@ -15,6 +15,7 @@ from utility.create_tensor_dataset_for_BC_from_dataframe import create_tensor_da
 import numpy as np
 from utility.masking_input import masking_input
 from utility.umBERT_trainer import umBERT_trainer
+import json
 
 # set the working directory to the path of the file
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -30,37 +31,56 @@ print("Catalogue loaded")
 
 # first step: obtain the embeddings of the dataset using the fine-tuned model finetuned_fashion_resnet18.pth
 
-# load the model finetuned_fashion_resnet18.pth
-print("Loading the model finetuned_fashion_resnet18.pth")
-fashion_resnet18 = resnet18()
-num_ftrs = fashion_resnet18.fc.in_features  # get the number of input features for the last fully connected layer
-fashion_resnet18.fc = nn.Linear(num_ftrs,
-                                4)  # modify the last fully connected layer to have the desired number of classes
-fashion_resnet18.load_state_dict(torch.load(
-    '../models/finetuned_fashion_resnet18.pth'))  # load the weights of the model finetuned_fashion_resnet18.pth
-fashion_resnet18.eval()  # set the model to evaluation mode
-fashion_resnet18.to(device)  # set the model to run on the device
+# # load the model finetuned_fashion_resnet18.pth
+# print("Loading the model finetuned_fashion_resnet18.pth")
+# fashion_resnet18 = resnet18()
+# num_ftrs = fashion_resnet18.fc.in_features  # get the number of input features for the last fully connected layer
+# fashion_resnet18.fc = nn.Linear(num_ftrs,
+#                                 4)  # modify the last fully connected layer to have the desired number of classes
+# fashion_resnet18.load_state_dict(torch.load(
+#     '../models/finetuned_fashion_resnet18.pth'))  # load the weights of the model finetuned_fashion_resnet18.pth
+# fashion_resnet18.eval()  # set the model to evaluation mode
+# fashion_resnet18.to(device)  # set the model to run on the device
+#
+# print("Model loaded")
+#
+# print("Loading the image dataset")
+#
+# # load the dataset (just for now, we will use the test dataset)
+# data_transform = transforms.Compose([  # define the transformations to be applied to the images
+#     transforms.Resize((224, 224)),  # resize the image to 224x224
+#     transforms.ToTensor()  # convert the image to a tensor
+# ])
+# data_dir = '../dataset_catalogue'  # define the directory of the dataset
+# image_dataset = CustomImageDataset(root_dir=data_dir, data_transform=data_transform)  # create the dataset
+#
+# dataloaders = DataLoader(image_dataset, batch_size=8, shuffle=False, num_workers=0)  # create the dataloader
+# print("Dataset loaded")
+#
+# print("Computing the embeddings of the dataset")
+# # get the embeddings of the dataset, the labels and the ids
+# embeddings, labels, IDs = image_to_embedding(dataloaders, fashion_resnet18, device)
+#
+# print("Embeddings computed")
 
-print("Model loaded")
+print("Saving the embeddings IDs")
 
-print("Loading the image dataset")
+# with open("../reduced_data/IDs_list", "w") as fp:
+#     json.dump(IDs, fp)
+with open("../reduced_data/IDs_list", "r") as fp:
+    IDs = json.load(fp)
 
-# load the dataset (just for now, we will use the test dataset)
-data_transform = transforms.Compose([  # define the transformations to be applied to the images
-    transforms.Resize((224, 224)),  # resize the image to 224x224
-    transforms.ToTensor()  # convert the image to a tensor
-])
-data_dir = '../dataset_catalogue'  # define the directory of the dataset
-image_dataset = CustomImageDataset(root_dir=data_dir, data_transform=data_transform)  # create the dataset
+print("IDs loaded")
 
-dataloaders = DataLoader(image_dataset, batch_size=8, shuffle=False, num_workers=0)  # create the dataloader
-print("Dataset loaded")
+# with open('../reduced_data/embeddings.npy', 'wb') as f:
+#     np.save(f, embeddings)
 
-print("Computing the embeddings of the dataset")
-# get the embeddings of the dataset, the labels and the ids
-embeddings, labels, IDs = image_to_embedding(dataloaders, fashion_resnet18, device)
+with open('../reduced_data/embeddings.npy', 'rb') as f:
+    embeddings = np.load(f)
 
-print("Embeddings computed")
+print("Embeddings loaded")
+
+
 
 # create MASK and CLS token embeddings as random tensors with the same shape of the embeddings
 CLS = np.random.rand(1, embeddings.shape[1])  # TODO controlla seed resti lo stesso
@@ -79,12 +99,12 @@ train_dataframe.drop(columns='compatibility', inplace=True)
 print("Creating the training set")
 training_set = create_tensor_dataset_for_BC_from_dataframe(train_dataframe, embeddings, IDs, CLS)
 training_dataset_BC = torch.utils.data.TensorDataset(training_set.transpose(0,1), torch.Tensor(compatibility_train))
-trainloader_BC = DataLoader(training_dataset_BC, batch_size=8, num_workers=0, shuffle=False)
+trainloader_BC = DataLoader(training_dataset_BC, batch_size=8, num_workers=0, shuffle=True)
 print("Training set for binary classification created")
 training_set_MLM, masked_positions_train, actual_masked_values_train = masking_input(training_set, train_dataframe,
                                                                                      MASK, with_CLS=False)
 training_dataset_MLM = torch.utils.data.TensorDataset(training_set_MLM, torch.Tensor(actual_masked_values_train))
-trainloader_MLM = DataLoader(training_dataset_MLM, batch_size=8, num_workers=0, shuffle=False)
+trainloader_MLM = DataLoader(training_dataset_MLM, batch_size=8, num_workers=0, shuffle=True)
 print("Training set for masked language model created")
 
 # import the validation set
@@ -95,12 +115,12 @@ valid_dataframe.drop(columns='compatibility', inplace=True)
 print("Creating the validation set")
 validation_set = create_tensor_dataset_for_BC_from_dataframe(valid_dataframe, embeddings, IDs, CLS)
 vallidation_dataset_BC = torch.utils.data.TensorDataset(validation_set.transpose(0,1), torch.Tensor(compatibility_valid))
-validloader_BC = DataLoader(vallidation_dataset_BC, batch_size=8, num_workers=0, shuffle=False)
+validloader_BC = DataLoader(vallidation_dataset_BC, batch_size=8, num_workers=0, shuffle=True)
 print("Validation set for binary classifiation created")
 validation_set_MLM, masked_positions_valid, actual_masked_values_valid = masking_input(validation_set, valid_dataframe,
                                                                                        MASK, with_CLS=False)
 validation_dataset_MLM = torch.utils.data.TensorDataset(validation_set_MLM, torch.Tensor(actual_masked_values_valid))
-validloader_MLM = DataLoader(validation_dataset_MLM, batch_size=8, num_workers=0, shuffle=False)
+validloader_MLM = DataLoader(validation_dataset_MLM, batch_size=8, num_workers=0, shuffle=True)
 print("Validation set for masked language model created")
 
 # import the test set
@@ -109,16 +129,16 @@ test_dataframe = pd.read_csv('../reduced_data/reduced_compatibility_test.csv')
 compatibility_test = test_dataframe['compatibility'].values
 test_dataframe.drop(columns='compatibility', inplace=True)
 
-print("Creating the test set")
-test_set = create_tensor_dataset_for_BC_from_dataframe(test_dataframe, embeddings, IDs, CLS)
-test_dataset_BC = torch.utils.data.TensorDataset(test_set.transpose(0,1), torch.Tensor(compatibility_test))
-testloader_BC = DataLoader(test_dataset_BC, batch_size=8, num_workers=0, shuffle=False)
-print("Test set for binary classifiation created")
-test_set_MLM, masked_positions_test, actual_masked_values_test = masking_input(test_set, test_dataframe, MASK,
-                                                                               with_CLS=False)
-test_dataset_MLM = torch.utils.data.TensorDataset(test_set_MLM, torch.Tensor(actual_masked_values_test))
-testloader_MLM = DataLoader(test_dataset_MLM, batch_size=8, num_workers=0, shuffle=False)
-print("Test set for masked language model created")
+# print("Creating the test set")
+# test_set = create_tensor_dataset_for_BC_from_dataframe(test_dataframe, embeddings, IDs, CLS)
+# test_dataset_BC = torch.utils.data.TensorDataset(test_set.transpose(0,1), torch.Tensor(compatibility_test))
+# testloader_BC = DataLoader(test_dataset_BC, batch_size=8, num_workers=0, shuffle=True)
+# print("Test set for binary classifiation created")
+# test_set_MLM, masked_positions_test, actual_masked_values_test = masking_input(test_set, test_dataframe, MASK,
+#                                                                                with_CLS=False)
+# test_dataset_MLM = torch.utils.data.TensorDataset(test_set_MLM, torch.Tensor(actual_masked_values_test))
+# testloader_MLM = DataLoader(test_dataset_MLM, batch_size=8, num_workers=0, shuffle=True)
+# print("Test set for masked language model created")
 
 # create the dictionary to work with modularity
 dataloaders_BC = {'train': trainloader_BC, 'val': validloader_BC}
@@ -130,7 +150,7 @@ masked_indices = {'train': masked_positions_train, 'val': masked_positions_valid
 optimizer = Adam(params=model.parameters(), lr=1e-4, betas=(0.9, 0.999), weight_decay=0.01)
 # put the model on the GPU
 model.to(device)
-criterion_1 = nn.BCELoss()
+criterion_1 = nn.BCEWithLogitsLoss()
 
 print('Start pre-training BC the model')
 

utility/umBERT_trainer.py
@@ -35,7 +35,7 @@ class umBERT_trainer():
 
                 for inputs,labels in dataloaders[phase]:  # for each batch
                     inputs = inputs.to(self.device)  # move the data to the device
-                    labels_BC,labels_MLM = labels
+                    labels_BC, labels_MLM = labels
                     labels_BC = labels_BC.to(self.device)  # move the data to the device
                     labels_MLM = labels_MLM.to(self.device)  # move the data to the device
 
@@ -96,12 +96,32 @@ class umBERT_trainer():
                     inputs = inputs.to(self.device)  # move the data to the device
                     labels = labels.to(self.device)  # move the data to the device
 
+
                     self.optimizer.zero_grad()  # zero the gradients
 
                     with torch.set_grad_enabled(
                             phase == 'train'):  # set the gradient computation only if in training phase
                         output = self.model.forward(inputs)  # compute the output of the model (forward pass)
                         clf = self.model.Binary_Classifier(output[:, 0, :])  # compute the logits
+                        # print(f'Shape of clf output : {clf.shape}')
+                        # print(f'output of clf is : {clf}')
+                        # clf will be the max value between the two final logits
+                        pred_labels = torch.max(self.model.sigmoid(clf), dim=1).indices  # compute the predicted labels
+                        # print(f'Shape of pred_labels : {pred_labels.shape}')
+                        # print(f'pred_labels is : {pred_labels}')
+                        pred_labels = pred_labels.unsqueeze(-1)
+                        # print(f'Shape of pred_labels after unsqueeze : {pred_labels.shape}')
+                        # print(f'pred_labels after unsqueeze is : {pred_labels}')
+                        # #TODO check that clf dimension is 8,2
+                        clf = torch.max(clf, dim=1).values # half the dimension to give the predicted logits to the loss
+                        # print(f'Shape of clf after max : {clf.shape}')
+                        clf = clf.unsqueeze(-1)
+                        # print(f'Shape of clf after unsqueeze : {clf.shape}')
+                        # print(f'Shape of input : {inputs.shape}')
+                        # print(f'Shape of labels : {labels.shape}')
+                        labels = labels.unsqueeze(-1)
+                        # print(f'Shape of labels after unsqueeze : {labels.shape}')
+                        # print(f'Shape of output : {clf.shape}')
                         loss = self.criterion(clf, labels)  # compute the loss
 
                         if phase == 'train':
@@ -109,13 +129,19 @@ class umBERT_trainer():
                             self.optimizer.step()  # update the parameters
 
                     running_loss += loss.item() * inputs.size(0)  # update the loss value (multiply by the batch size)
-                    accuracy += torch.sum(torch.argmax(clf, dim=1) == labels)  # update the accuracy
+                    # the accuracy must be computed on the activated output
+                    # print(f'Predicted labels : {pred_labels}')
+                    # print(f'Labels : {labels}')
+                    # print(f'The sum of correct predictions {torch.sum(pred_labels == labels)}')
+                    accuracy += torch.sum(pred_labels == labels)  # update the accuracy
+                    # print(f'Accuracy : {accuracy:10f}')
 
+                print(f'length of dataset is : {len(dataloaders[phase].dataset)}')
                 epoch_loss = running_loss / len(dataloaders[phase].dataset)  # compute the average loss of the epoch
                 epoch_accuracy = accuracy / len(dataloaders[phase].dataset)  # compute the average accuracy of the epoch
 
-                print(f'{phase} Loss: {epoch_loss}')
-                print(f'{phase} Accuracy : {epoch_accuracy}')
+                print(f'{phase} Loss: {epoch_loss:.10f}')
+                print(f'{phase} Accuracy : {epoch_accuracy:.10f}')
 
     def pre_train_MLM(self, dataloaders, masked_positions):  # labels sono le posizioni dei vestiti nel catalogo, precedentemente calcolate da masking input
         """