[WIP] fixed accuracy computation

BERT_architecture/umBERT3.py
@@ -64,10 +64,8 @@ class umBERT3(nn.Module):
         :return: the outfits with the CLS token, a tensor of shape (batch_size, seq_len+1, d_model)
         """
         # create a tensor of shape (batch_size, seq_len+1, d_model)
-        new_inputs = torch.zeros((inputs.shape[0], inputs.shape[1] + 1, inputs.shape[2])).to(device)
-        for i in range(inputs.shape[0]):  # for each outfit in inputsb
-            new_inputs[i] = torch.cat((self.CLS, inputs[i]), 0)  # add the CLS token at the beginning of the outfit
-        return new_inputs
+        CLS = self.CLS.repeat(inputs.shape[0], 1, 1)
+        return  torch.cat((CLS,inputs), dim=1)
 
     def mask_random_item(self, inputs):
         """

nuovi_embeddings/generate_embeddings_for_autoencoder_ssimval.py
@@ -17,7 +17,7 @@ print("Device used: ", device)
 # define the size of the embeddings
 
 # load thecheckpoint
-checkpoint = torch.load(f'./2023_07_24_trained_fashion_VAE_semplice.pth',map_location=torch.device('cpu'))
+checkpoint = torch.load(f'./2023_07_25_trained_fashion_VAE_con_linear_layers.pth')  # map_location=torch.device('cpu')
 # load the model finetuned_fashion_resnet18_512.pth
 print(f"Loading the trained model")
 model = AutoEncoder()
@@ -49,10 +49,10 @@ print("Computing the embeddings of the dataset")
 embeddings, IDs = image_to_embedding_ANTONIO(dataloaders, model, device)
 print("Embeddings computed")
 print("Saving the embeddings IDs")
-with open("AESSIMVAL_new_IDs_list", "w") as fp:
+with open("AESSIMVAL_with_linear_layers_new_IDs_list", "w") as fp:
     json.dump(IDs, fp)
 print("IDs saved")
 dim_embeddings = embeddings.shape[1]
-with open(f'./SCIUE_new_embeddings_{dim_embeddings}.npy', 'wb') as f:
+with open(f'./SCIUE_with_linear_layers_new_embeddings_1024.npy', 'wb') as f:
     np.save(f, embeddings)
 print("Embeddings saved")

nuovi_embeddings/image_to_embeddings_AESSIMVAL.py
@@ -12,7 +12,7 @@ def image_to_embedding_ANTONIO(dataloader, model, device):
     # get the embeddings
     embeddings = []  # list to store the embeddings
     with torch.no_grad():  # do not calculate the gradients
-        for inputs, _ in tqdm(dataloader):  # iterate over the data
+        for inputs, _ in tqdm(dataloader,colour='blue'):  # iterate over the data
             inputs = inputs.to(device)  # move the input images to the device
             _,outputs = model(inputs)  # forward pass
             embeddings.append(outputs.reshape(outputs.shape[0],-1).cpu().numpy())  # append the embeddings to the list

nuovi_embeddings/main_autoencoder_ssimval.py
@@ -1,67 +1,70 @@
+# import required libraries to train an autoencoder model (used later to compute image embeddings)
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 from torchvision.datasets import ImageFolder
 from modello_autoencoder_ssimval import AutoEncoder, fine_tune_model, test_model, freeze, SSIM_Loss
 import torch
+
+# use GPU if available (mps is mac M1 pro GPU)
 device = torch.device("mps" if torch.backends.mps.is_built() else "cpu")
 # device = torch.device('cpu')
 print(f"Working on device {device}")
 
+# define the transformations to be applied to the images, the employed model doesn't require normalization
 data_transforms = {
     'train': transforms.Compose(
-        [  # For training, the data gets transformed by undergoing augmentation and normalization.
+        [  # For training, the data gets transformed by undergoing augmentation.
             transforms.RandomCrop(size=128, pad_if_needed=True),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.ToTensor(),
         ]),
     'val': transforms.Compose([
-        transforms.Resize(size=128),# Just normalization for validation, no augmentation.
+        transforms.Resize(size=128),
         transforms.ToTensor()
     ]),
     'test': transforms.Compose([
-        transforms.Resize(size=128), # Just Resizing for testing, no normalization and no augmentation.
+        transforms.Resize(size=128),
         transforms.ToTensor()
     ])
 }
 
 # Set the paths to your training and validation data folders
-data_dir = './dataset_cnn_fine_tuning'
+data_dir = '../dataset_cnn_fine_tuning'
 train_dir = data_dir + '/train'
 val_dir = data_dir + '/val'
 test_dir = data_dir + '/test'
 
 # Create the ImageFolder datasets for training and validation
+print("Loading the datasets")
 image_datasets = {
     'train': ImageFolder(train_dir, transform=data_transforms['train']),
     'val': ImageFolder(val_dir, transform=data_transforms['val']),
     'test': ImageFolder(test_dir, transform=data_transforms['test'])
 }
+print("Datasets loaded")
 
 # Create data loaders for training and validation
+print("Creating the dataloaders")
 dataloaders = {
     'train': DataLoader(image_datasets['train'], batch_size=32, shuffle=True, num_workers=0),
     'val': DataLoader(image_datasets['val'], batch_size=32, shuffle=False, num_workers=0),
     'test': DataLoader(image_datasets['test'], batch_size=32, shuffle=False, num_workers=0)
 }
-
-# Load the pre-trained ResNet18 model and modify the last fully connected layer
-# train the VAE based on ResNet18
-# define the model and the optimizer
+print("Dataloaders created")
+# define the model, the optimizer and a criterion (loss function)
 model = AutoEncoder(C=128, M=128, in_chan=3, out_chan=3).to(device)
 
 optimizer = torch.optim.Adam(model.parameters(),
-                                    lr=1e-4, weight_decay=1e-5)
-#
+                             lr=1e-4, weight_decay=1e-5)
+
 criterion = SSIM_Loss()
 
-# modify the model architecture to output embeddings of the given dimension
-# and classify the embeddings into the given number of classes
 num_epochs = 50  # number of epochs to train the model
 
-
-# fine-tune the model
-model = fine_tune_model(model=model, freezer=freeze, optimizer=optimizer, dataloaders=dataloaders, device=device,criterion=criterion, n_layers_to_freeze=0,  num_epochs=num_epochs)
+# train the model
+model = fine_tune_model(model=model, freezer=freeze, optimizer=optimizer, dataloaders=dataloaders, device=device,
+                        criterion=criterion, n_layers_to_freeze=0, num_epochs=num_epochs)
 
 # test the model
 test_model(model, dataloaders['test'], device, criterion)

nuovi_embeddings/modello_autoencoder_ssimval.py
@@ -1,22 +1,19 @@
+# import the required libraries to define an autoencoder model as seen in the repository https://github.com/VainF/pytorch-msssim/tree/master
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 from tqdm import tqdm
 import numpy as np
-
-# define a loss based on ssimval
-class SSIM_Loss(SSIM):
-    def forward(self, img1, img2):
-        return 100*( 1 - super(SSIM_Loss, self).forward(img1, img2) )
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.autograd import Function
 
-
+# define a loss based on ssimval
+class SSIM_Loss(SSIM):
+    def forward(self, img1, img2):
+        return 100*( 1 - super(SSIM_Loss, self).forward(img1, img2) )
 
 class LowerBound(Function):
     @staticmethod
@@ -85,17 +82,18 @@ class AutoEncoder(nn.Module):
         super(AutoEncoder, self).__init__()
         self.encoder = Encoder(C=C, M=M, in_chan=in_chan)
         self.decoder = Decoder(C=C, M=M, out_chan=out_chan)
+        # add linear layers to reduce the dimensionality of the hidden representation
         self.ffnn1 = nn.Linear(128*8*8, 1024)
         self.ffn2 = nn.Linear(1024, 128*8*8)
 
     def forward(self, x, **kargs):
         code = self.encoder(x)  # shape (batch_size, 128, 8, 8)
-        code = code.reshape(code.shape[0], -1) # flatten the hidden representation
+        code = code.reshape(code.shape[0], -1)  # flatten the hidden representation
         code = self.ffnn1(code)  # the hidden representation we will return
         out = self.ffn2(code)
         out = out.reshape(out.shape[0], 128, 8, 8)
         out = self.decoder(out)
-        return out, code
+        return out, code  # return the reconstructed image and the hidden representation
 
 
 class Encoder(nn.Module):
@@ -150,10 +148,11 @@ class Decoder(nn.Module):
 # freeze the given number of layers of the given model
 def freeze(model, n=None):
     """
-    this function freezes the given number of layers of the given model in order to fine-tune it.
+    This function freezes the given number of layers of the given model in order to fine-tune it.
     :param model: the model to be frozen
     :param n: the number of layers to be frozen, if None, all the layers are frozen
     :return: the frozen model
+    #TODO non usata si può togliere
     """
     if n is None:
         for param in model.parameters():
@@ -176,6 +175,7 @@ def fine_tune_model(model, freezer, optimizer, dataloaders, device, criterion, n
     :param device: the device to be used
     :param num_epochs: the number of epochs to train the model
     :return: the fine-tuned model
+    # TODO cambia il nome qua
     """
 
     # freeze parameters of the pre-trained model
@@ -207,7 +207,7 @@ def fine_tune_model(model, freezer, optimizer, dataloaders, device, criterion, n
                 optimizer.zero_grad()  # zero the parameter gradients
 
                 with torch.set_grad_enabled(phase == 'train'):  # only calculate the gradients if training
-                    outputs = model(inputs)  # forward pass
+                    outputs,_ = model(inputs)  # forward pass
                     ssm_loss = criterion(outputs, inputs)
                     loss = ssm_loss  # calculate the loss
 
@@ -233,11 +233,12 @@ def fine_tune_model(model, freezer, optimizer, dataloaders, device, criterion, n
                     torch.save(checkpoint, f'./{dt_string}_trained_fashion_VAE_con_linear_layers.pth')
                     valid_loss_min = epoch_loss
                     best_model = model
+                    early_stopping = 0
                 else:
                     early_stopping += 1
-            if early_stopping == 5:
-                print('Early stopping!')
-                break
+        if early_stopping == 5:
+            print('Early stopping!')
+            break
     plt.plot(train_loss, label='train_total')
     plt.plot(val_loss, label='val_total')
     plt.legend()